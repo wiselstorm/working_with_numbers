f3fb86bdef2db9e99b295278ae1adf34a6feb777
Timotej Nidorfer <58996951+wiselstorm@users.noreply.github.com>
2022-09-29 20:14:18 +0200

    Add files via upload
    A rudimentary, barebones(without functions) program to seperate numbers into two halves

---
+++ seperate_numbers_in_halves.py
@@ -0,0 +1,12 @@
+x=input("Enter first number\n")
+x=int(x)
+#y=input("Enter second number\n")
+#y=int(y)
+q1=x/100 #quotient of the first number
+
+rem1=x%100
+f1=int(round(rem1,2))
+f2=int(round(q1,2))
+a=f2
+b=f1
+print(a," ",b)
